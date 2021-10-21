fc2097a6284dfa625449ae142e9d932dd1b6d349
Rishabh Jain <62394291+r-rishabh-j@users.noreply.github.com>
2021-10-21 21:53:44 +0530

    create-student

--- setup.sql
+++ setup.sql
@@ -10,14 +10,30 @@ create schema academic_data;
 create schema course_offerings;
 create schema student_grades;
 
+create table academic_data.departments
+(
+    dept_name varchar primary key
+);
+
+insert into academic_data.departments values('cse');
+
+create table academic_data.degree
+(
+    degree varchar primary key
+);
+
+insert into academic_data.degree values('btech');
+
 create table academic_data.course_catalog
 (
     course_code        varchar primary key,
+    dept_name          varchar not null,
     credits            integer not null,
     credit_structure   varchar not null,
     course_description varchar   default '',
     pre_requisites     varchar[] default '{}',
-    introduced_on      date    not null
+    introduced_on      date    not null,
+    foreign key (dept_name) references academic_data.departments (dept_name)
 );
 
 create table academic_data.student_info
@@ -25,12 +41,23 @@ create table academic_data.student_info
     roll_number  varchar primary key,
     student_name varchar not null,
     department   varchar not null,
-    course       varchar not null,
+    degree       varchar not null,
     batch_year   integer not null,
-    contact      varchar
+    contact      varchar,
+    foreign key (department) references academic_data.departments (dept_name),
+    foreign key (degree) references academic_data.degree (degree)
 );
 
-create function course_offerings.add_new_semester(academic_year integer, semester_number integer) returns void as
+create table academic_data.faculty_info
+(
+    faculty_id   varchar primary key,
+    faculty_name varchar not null,
+    department   varchar not null,
+    contact      varchar,
+    foreign key (department) references academic_data.departments (dept_name)
+);
+
+create or replace function course_offerings.add_new_semester(academic_year integer, semester_number integer) returns void as
 $function$
 begin
     execute ('create table course_offerings.sem_' || academic_year || '_' || semester_number || '
@@ -43,4 +70,24 @@ begin
                 );'
         );
 end;
+$function$ language plpgsql;
+
+create or replace function create_student(
+    roll_number varchar, student_name varchar, department varchar, 
+    degree varchar, batch_year integer, contact varchar
+    ) 
+returns void as
+$function$
+begin
+    insert into academic_data.student_info values (roll_number, student_name, department, degree, batch_year, contact);
+    execute ('create table student_grades.' || roll_number || '
+                (
+                    course_code     varchar not null,
+                    semester        integer not null,
+                    year            integer not null,
+                    grade           varchar not null,
+                    foreign key (course_code) references academic_data.course_catalog (course_code)
+                );'
+        );
+end
 $function$ language plpgsql;
