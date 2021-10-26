cedfd9d3210a25d3a85202fdba5e3886901d4456
Rishabh Jain <62394291+r-rishabh-j@users.noreply.github.com>
2021-10-26 22:12:06 +0530

    student, faculty fns

--- faculty_generator.py
+++ faculty_generator.py
@@ -2,7 +2,7 @@ import csv
 import random
 import string
 
-fields = ['Faculty_ID', 'Faculty_name', 'Department', 'Contact']
+fields = ['Faculty_ID', 'Faculty_name', 'Department']
 departments = ['cse', 'me', 'ee']
 
 rows = []
@@ -13,7 +13,6 @@ for i in range(num_rows):
     record.append(random.choice(departments) + ''.join(random.choice(string.digits) for _ in range(4)))
     record.append(''.join(random.choice(string.ascii_uppercase) for _ in range(8)))
     record.append(random.choice(departments))
-    record.append(''.join(random.choice(string.digits) for _ in range(10)))
     rows.append(record)
 
 with open('faculty_info.csv', 'w') as csvfile:
